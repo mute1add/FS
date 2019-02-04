d2b7260ab52d96e46c808acd7ed52c5a51e87c98
Roman Ivashchuk <ivaschuk14@gmail.com>
2019-02-04 13:32:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -380,7 +380,7 @@
 //
 // })
 let prevDropOpened = null;
-$(".circle-elements").on("click", function (event) {
+$(".spoiler-header").on("click", function (event) {
     console.log(event.target);
     console.log(document.getElementById(event.target.getAttribute('dropid')));
     let dropdown = $(`#${event.target.getAttribute('dropid')}`);
